a44da42449bc34c3660a463dba249fa38a89143c
Avishay Balter <avishaybalter@Avishays-MacBook-Pro.local>
2018-01-22 22:26:27 +0200

    fix all scripts

--- Artifacts/UbuntuChrome/ubuntu_install_chrome_v1.sh
+++ Artifacts/UbuntuChrome/ubuntu_install_chrome_v1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install xfce4
-sudo apt-get install xrdp
-echo xfce4-session >~/.xsession
-sudo service xrdp restart
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
--- Artifacts/UbuntuPython352/ubuntu_install_python352_v1.sh
+++ Artifacts/UbuntuPython352/ubuntu_install_python352_v1.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install xfce4
-sudo apt-get install xrdp
-echo xfce4-session >~/.xsession
-sudo service xrdp restart
+sudo apt-get install build-essential checkinstall
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+version=3.5.2
+cd ~/Downloads/
+wget https://www.python.org/ftp/python/$version/Python-$version.tgz
+tar -xvf Python-$version.tgz
+cd Python-$version
+./configure
+make
+sudo checkinstall
